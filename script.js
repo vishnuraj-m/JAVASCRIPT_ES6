2a1f7ee40295477c7d660cc5fe198f48aa0e7ee9
vishnuacappella <acappella.studiov@gmail.com>
2022-07-13 18:24:26 +0530

    completed

--- script.js
+++ script.js
@@ -8,3 +8,27 @@ const great1 = function (Name = 'Vishnu') {
     console.log('hi ' + Name);
 }
 great1();
+
+// !arrow Function
+const add = function (a = 0, b = 0) {
+    return a + b
+}
+console.log(add(1, 3));
+
+const addArrow = (a = 0, b = 0) => {
+    return a + b;
+}
+console.log(addArrow(10, 3));
+
+// *arrow Function one line
+const addArrow1 = (a = 5, b = 7) => a + b;
+console.log(addArrow1(10, 5));
+
+// !arrow function in map
+
+const arr = [1, 2, 3, 4, 5];
+const arr2 = arr.map((item) => item);
+console.log(arr2);
+
+const sum = arr.reduce((a, b) => a + b)
+console.log(sum);
